6b053343fb76dcf5c70838c2823bb69c5d462416
harryra18 <harryra18@gmail.com>
2025-09-15 20:02:10 -0400

    second commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.send('The server is running');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Listening on port 3000');
   console.log(`Your secret is ${process.env.SECRET_PASSWORD}`);
 });
